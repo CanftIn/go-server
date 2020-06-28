b7643101a9ca0722c787c3d54f038b8c22b746a6
CanftIn <wwc7033@gmail.com>
2020-06-28 17:05:53 +0800

    add Request module

---
+++ znet/request.go
@@ -0,0 +1,22 @@
+package znet
+
+import "CanftIn/go-server/ziface"
+
+type Request struct {
+	// 已经和客户端建立好的连接
+	conn ziface.IConnection
+
+	// 客户端请求的数据
+	data []byte
+}
+
+// 得到当前连接
+func (r *Request) GetConnection() ziface.IConnection {
+	return r.conn
+}
+
+// 得到当前的请求数据
+func (r *Request) GetData() []byte {
+	return r.data
+}
+
